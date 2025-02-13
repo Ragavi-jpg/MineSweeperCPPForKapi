702d40e3941a57dfa36dfbaf36017fe3152db252
Ragavi-jpg <asriel4k@gmail.com>
2025-02-13 22:12:07 +0100

    Started Project
    Started the project for the minesweeper for valentines day.
    
    Currently have working:
    -A visual representation of the game on the console
    -A way of selecting a box in the board
    -A way of putting a flag or digging
    -When you dig a box with a bomb you loose and the program ends
    
    To do:
    -a way of show all contiguos free boxes when and the contiguos numbered boxes to those when one free box is digged
    -A way of calculating the numbered boxes number Or update the array "hubicacionBombas" to contain said numbers.
    -Code winning

---
+++ BuscaminasPersonalizado/Buscaminas.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+
+using namespace std;
+
+const int DIMENSIONX = 10;
+const int DIMENSIONY = 11;
+
+const int hubicacionBombas[DIMENSIONX][DIMENSIONY] = {
+    {0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,1,1,0},
+    {0,0,0,0,1,1,0,0,0,1,0},
+    {0,0,0,0,0,1,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,1,0,0,0,0,0,1,0},
+    {0,0,0,0,0,0,0,0,1,1,0},
+    {0,1,0,0,0,0,0,0,0,0,0},
+    {0,1,0,0,1,0,0,1,1,1,0},
+    {0,0,0,0,0,0,0,0,0,0,0}
+};
+
+
+int contarBombas()
+{
+    int n = 0;
+    for(int i = 0; i<DIMENSIONX; i++){
+        for(int j = 0; j<DIMENSIONY; j++){
+            if(hubicacionBombas[i][j] == 1) n++;
+        }
+    }
+    return n;
+}
+
+void imprimir(char matriz[DIMENSIONX+1][DIMENSIONY+1])
+{
+    for(int i = 0; i<DIMENSIONX+1; i++){
+        for(int j = 0; j<DIMENSIONY+1; j++){
+            cout<<matriz[i][j]<<' '
+            ;
+        }
+        cout<<'\n';
+    }
+    cout<<endl;
+}
+
+void jugar(char matriz[DIMENSIONX+1][DIMENSIONY+1])
+{
+    int ganarOPerder = 0; //ganar = 1, perder = -1
+    int fila;
+    int columna;
+    bool hubicacionCorrecta;
+    char colocarBandera;
+
+    while(ganarOPerder == 0){
+        do{
+            imprimir(matriz);
+            if(!hubicacionCorrecta) cout<<"Casilla incorrecta, prueba otra vez"<<'\n';
+            hubicacionCorrecta = true;
+            cout<<"Colocar bandera?(Y/N): ";
+            cin>>colocarBandera;
+            if(colocarBandera == 'Y' || colocarBandera == 'y'){
+                cout<<"Escoge Número de fila: ";
+                cin>>fila;
+                cout<<"Escoge Número de columna: ";
+                cin>>columna;
+                system("cls");
+                if(fila<0 || fila>=DIMENSIONX-2 || columna<0 || columna>=DIMENSIONY-2) hubicacionCorrecta = false;
+                if(hubicacionCorrecta) matriz[fila+1][columna+1] = 'O';
+            }else{
+                cout<<"Escoge Número de fila: ";
+                cin>>fila;
+                cout<<"Escoge Número de columna: ";
+                cin>>columna;
+                system("cls");
+                if(fila<0 || fila>=DIMENSIONX-2 || columna<0 || columna>=DIMENSIONY-2) hubicacionCorrecta = false;
+            }
+        }while(!hubicacionCorrecta);
+        if(hubicacionBombas[fila+1][columna+1] == 1 && !(colocarBandera == 'y' || colocarBandera == 'Y')){
+            matriz[fila+1][columna+1] = 'X';
+            ganarOPerder = -1;
+        }
+    }
+    if(ganarOPerder == 1){
+        imprimir(matriz);
+        cout<<"[ ¡¡GANASTE!! ]"<<endl;
+    }else{
+        imprimir(matriz);
+        cout<<"[ PERDISTE D: ]"<<endl;
+    }
+}
+
+int main()
+{
+    int nBombas = contarBombas();
+    char m = '#', e = '.', b = ' ';
+    char tablero[DIMENSIONX+1][DIMENSIONY+1] = {
+    {m,m,m,m,m,m,m,m,m,m,m,'F'},
+    {m,e,e,e,e,e,e,e,e,e,m,'0'},
+    {m,e,e,e,e,e,e,e,e,e,m,'1'},
+    {m,e,e,e,e,e,e,e,e,e,m,'2'},
+    {m,e,e,e,e,e,e,e,e,e,m,'3'},
+    {m,e,e,e,e,e,e,e,e,e,m,'4'},
+    {m,e,e,e,e,e,e,e,e,e,m,'5'},
+    {m,e,e,e,e,e,e,e,e,e,m,'6'},
+    {m,e,e,e,e,e,e,e,e,e,m,'7'},
+    {m,m,m,m,m,m,m,m,m,m,m,b},
+    {'C','0','1','2','3','4','5','6','7','8',b,b}
+    };
+
+    setlocale(LC_ALL, "");
+    jugar(tablero);
+    return 0;
+}
+
+/*
+    cosas por hacer:
+        {
+        -Función que calcule las casillas contiguas libres cuando excaves una
+        -Función que ponga en blanco las casillas libres cuando excaves una
+        -Función que muestre los números de las minas cuando excaves casillas libres
+        }
+        -Función que calcule los números O Actualizar la hubicación de las minas para que también tengan los números
+*/
